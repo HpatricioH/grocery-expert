2af11604a3375fa325024345b986f6a391cad1ee
HPHuerta <hhernanp@gmail.com>
2022-12-23 01:39:40 -0500

    first commit

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,6 @@
+export const Login = () => {
+  const test = "this is a test"
+  return (
+    <div>Login Page for Expert Grocery</div>
+  )
+}
---
+++ src/tests/Components/Login.test.jsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import { Login } from '../../Components/Login/Login'
+
+describe('Test Login', () => [
+  it('Renders Login Component Correctly', () => {
+    const component = render(<Login />, { wrapper: BrowserRouter })
+
+    const textInPage = component.getByText('Login Page for Expert Grocery')
+
+    expect(textInPage).toBeInTheDocument()
+  })
+])
