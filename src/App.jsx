2ea79a8eac10fd15dd5ff497cfa7e57904101648
HPHuerta <hhernanp@gmail.com>
2023-01-07 00:28:11 -0500

    refactor: change UserProvider to app

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,24 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home } from './Pages/Home/Home'
 import { Login } from './Pages/Login/Login'
 import { SignUp } from './Pages/SignUp/SignUp'
+import { UserProvider } from './context/UserContext'
 import React from 'react'
 
 function App () {
   return (
-    <>
+    <UserProvider>
       <CssBaseline />
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/signUp' element={<SignUp />} />
-          <Route element={<PrivateRoutes />}>
+          <Route path='/home' element={<PrivateRoutes />}>
             <Route path='/home' element={<Home />} exact />
           </Route>
         </Routes>
       </BrowserRouter>
 
-    </>
+    </UserProvider>
   )
 }
 
