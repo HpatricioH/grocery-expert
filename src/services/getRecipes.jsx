148439f25175a299f753102e946ee00bf8a2fd6e
HPHuerta <hhernanp@gmail.com>
2023-02-18 00:49:24 -0500

    refactor: ingredientRecipes component refactor and added hooks and service to add to favorites

--- src/services/getRecipes.jsx
+++ src/services/getRecipes.jsx
@@ -3,8 +3,6 @@ import axios from 'axios'
 export const getRecipes = async (groceryName) => {
   const URL = `https://www.themealdb.com/api/json/v2/9973533/filter.php?i=${groceryName}`
 
-  console.log(groceryName)
-
   try {
     const { data } = await axios.get(`${URL}`)
     return data
--- src/utilities/modal.jsx
+++ src/utilities/modal.jsx
@@ -2,18 +2,18 @@ import { useState } from 'react'
 
 export const modal = () => {
   const [open, setOpen] = useState(false)
-  const [singleProduct, setSingleProduct] = useState(null)
+  const [idRecipe, setIdRecipe] = useState(null)
 
   const handleClose = () => {
     setOpen(false)
-    setSingleProduct(null)
+    setIdRecipe(null)
   }
 
-  const handleOpen = (id, groceries) => {
-    const grocery = groceries?.filter((el) => el.id === id)
-    setSingleProduct(grocery)
+  const handleOpen = (id, recipes) => {
+    const singleRecipe = recipes?.meals?.filter((el) => el.idMeal === id)
+    setIdRecipe(singleRecipe[0].idMeal)
     setOpen(true)
   }
 
-  return { handleOpen, handleClose, open, singleProduct }
+  return { handleOpen, handleClose, open, idRecipe }
 }
