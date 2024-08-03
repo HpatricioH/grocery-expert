60a06558adcbebac8608c4ba34804ed4c41e0f8c
HPHuerta <hhernanp@gmail.com>
2024-08-03 01:37:21 -0400

    chore: Update font family to use Montserrat in tailwind.config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -7,7 +8,7 @@ export default {
         'primary-color': '#eff9f0',
       },
 			fontFamily: {
-        montserrat: ["montserrat", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
       },
 		},
 	},
