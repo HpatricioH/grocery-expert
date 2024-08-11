63d15326c686cd482ef4555b0103806279a23aa6
HPHuerta <hhernanp@gmail.com>
2024-08-10 23:24:47 -0400

    refactor: Add Pantry page and configure authentication

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import icon from "astro-icon"
+import auth from "auth-astro"
+import vercel from "@astrojs/vercel/serverless"
 
-import icon from "astro-icon";
+import db from "@astrojs/db"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
-});
+  integrations: [tailwind(), icon(), auth(), db()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  })
+})
