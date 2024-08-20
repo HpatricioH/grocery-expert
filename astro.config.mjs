366f4e41dedd8c8dbd2309974f53dd4f2d16f18d
HPHuerta <hhernanp@gmail.com>
2024-08-20 00:00:25 -0400

    refactor: Update Categories component to display different categories

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import icon from "astro-icon"
 import auth from "auth-astro"
-import vercel from "@astrojs/vercel/serverless"
-
 import db from "@astrojs/db"
+
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
