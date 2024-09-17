5054fa4f317d1d94c5c64d05bf27c088b2615729
HPHuerta <hhernanp@gmail.com>
2024-09-17 00:02:17 -0400

    fix: Remove unused @astrojs/db integration from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import icon from "astro-icon"
 import auth from "auth-astro"
-import db from "@astrojs/db"
 
 import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), auth(), db()],
+  integrations: [tailwind(), icon(), auth()],
   output: "server",
   adapter: vercel({
     webAnalytics: {
