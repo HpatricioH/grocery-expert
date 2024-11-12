7e1650fe3e899d229ce5a7d45c892d95d9867d7f
HPHuerta <hhernanp@gmail.com>
2024-11-11 23:54:29 -0500

    feat: Add search functionality with input sanitization and recipe data retrieval

---
+++ src/pages/search.json.ts
@@ -0,0 +1,25 @@
+import { recipes } from "../lib/data/recipes.json"
+
+interface Recipe {
+  title: string
+  category: string
+  cookTime: string
+}
+
+function getRecipes(): Recipe[] {
+  const recipesData: Recipe[] = recipes.map((recipe) => ({
+    title: recipe.title,
+    category: recipe.category,
+    cookTime: recipe.cookTime
+  }))
+  return recipesData
+}
+
+export function GET() {
+  return new Response(JSON.stringify(getRecipes()), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    }
+  })
+}
